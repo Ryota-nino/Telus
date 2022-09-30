ef636f6472ea8a1b0bac79d28d2d1a1fd3a4c0fb
Ryota-nino <ryota.swimmer@icloud.com>
2022-09-30 15:37:49 +0900

    Initial Commit

---
+++ Telus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Telus
+//
+//  Created by 二宮良太 on 2022/09/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Telus/TelusApp.swift
@@ -0,0 +1,17 @@
+//
+//  TelusApp.swift
+//  Telus
+//
+//  Created by 二宮良太 on 2022/09/30.
+//
+
+import SwiftUI
+
+@main
+struct TelusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
